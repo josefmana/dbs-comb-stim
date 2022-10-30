f64659e5ba3ebc3bb20bff7373d15ab0e4bdc300
josefmana <josef.mana@protonmail.com>
2022-10-30 22:39:33 +0100

    counterbalancing

--- dbs_lowFREQ_project_building.R
+++ dbs_lowFREQ_project_building.R
@@ -10,6 +10,10 @@ for ( i in pkgs ) {
   if ( i %in% names( sessionInfo()$otherPkgs ) == F ) library( i , character.only = T ) # load if it ain't loaded yet
 }
 
+# create folders "figures", "tables" and "sessions" to store results and sessions info in
+# prints TRUE and creates the folder if it was not present, prints NULL if the folder was already present.
+sapply( c("figures", "tables", "sessions"), function(i) if( !dir.exists(i) ) dir.create(i) )
+
 
 # ---- counterbalancing ----
 
@@ -21,10 +25,11 @@ int <- c("low", "base") # the interim, either combined low-ventral and high-dors
 # generate all counterbalancing conditions across sessions and measurements
 cb.long <- expand.grid(s1,int,s2) %>%
   `colnames<-`( c("session_1", "interim", "session_2") ) %>%
-  mutate( cb_long = paste0( "cond_", 1:nrow(.) ),
-          change_freq = ifelse( session_1 == session_2, 0, 1)
-          )
+  write.table( . , file = "tables/counterbalancing.csv", sep = ",", row.names = F )
 
-# list all counterbalancing conditions for the cognitive assessments
-block <- c("srt/vf", "vf/srt")
+
+# ----------- session info -----------
+
+# write the sessionInfo() into a .txt file
+capture.output( sessionInfo(), file = "sessions/dbs_lowFREQ_project_building.txt" )
 
