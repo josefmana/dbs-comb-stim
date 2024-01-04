169f374a4d95bdb97fd363dd59ad1e95450019fa
Josef Mana <josef.mana@protonmail.com>
2024-01-04 20:37:00 +0100

    No-signal/go trials summary ...

--- scripts/ssrt_desc.R
+++ scripts/ssrt_desc.R
@@ -49,10 +49,8 @@ d0 <- #d1 <-
 # step no.2: do some basic design checks & basic performance ----
 
 # check design
-with( d0, table( id, signal, sess ) ) # signal in 1/4 of trials
-with( d0, table( id, stim, sess ) ) # the same number of rights and lefts
-#with( d1, table( id, signal, sess ) )
-#with( d1, table( id, stim, sess ) )
+with( d1, table( id, signal, sess ) ) # signal in 1/4 of trials
+with( d1, table( id, stim, sess ) ) # the same number of rights and lefts
 
 # check overall error rates of all subjects on both no-signal & signal trials
 # choosing error rates over accuracy rates for two reasons:
@@ -90,14 +88,50 @@ t1 <-
 
   )
 
+# save it
+write.table( t1, here("tabs","error_tab.csv"), sep = ",", row.names = F, quote = F )
 
 
-# check overall accuracy of all subjects on no-signal trials
-with( subset( d0, signal == "nosignal" ), round( 200 * prop.table( table( id, acc, sess ), margin = 1 ), 0 ) )
-#with( subset( d1, signal == "nosignal" ), round( 200 * prop.table( table( id, acc, sess ), margin = 1 ), 0 ) )
-#with( subset( d1, signal == "nosignal" ), round( 200 * prop.table( table( id, acc, cond ), margin = 1 ), 0 ) )
+# step no. 3: analyse no-signal data ----
 
-# check overall accuracy of all subjects on signal trials
-with( subset( d0, signal == "signal" ), round( 200 * prop.table( table( id, acc, sess ), margin = 1 ), 0 ) )
-#with( subset( d1, signal == "signal" ), round( 200 * prop.table( table( id, acc, sess ), margin = 1 ), 0 ) )
-#with( subset( d1, signal == "signal" ), round( 200 * prop.table( table( id, acc, cond ), margin = 1 ), 0 ) )
+# prepare data sets of both no-signal and signal trials only pivoting longer for acc, miss and rt1 variables
+#for( i in 2:3 ) assign(
+#  
+#  paste0("d",i), # d2 for no-signal/go data, d3 for signal/stop data
+#  
+#  d1 %>%
+#    filter( signal == case_when( i == 2 ~ "nosignal", i == 3 ~ "signal" ) ) %>%
+#    pivot_longer( cols = c("acc","miss","rt1"), names_to = "var", values_to = "val" ) %>%
+#    select( id, cond, correct, presp, var, val )
+#  
+#)
+
+# accuracy, i.e. patient/condition specific p(correct)
+# trials without a response or anticpatory responses (i.e., presp == 0) are omitted
+#acc <- d2 %>% filter( var == "acc" & presp == 1 ) %>% group_by( id, cond ) %>% summarise( `p(correct)` = mean(val) )
+
+# miss rate, i.e., patient/condition specific p(miss)
+#mis <- d2 %>% filter( var == "miss" ) %>% group_by( id, cond ) %>% summarise( `p(miss)` = mean(val) )
+
+# RTs for correct responses
+#rt <- d2 %>% filter( var == "rt1" ) %>% group_by( id, exp ) %>% summarise( mean_rt = mean(val), sd_rt = sd(val) )
+
+# prepare a table summarising accuracy, miss rate and response times of no-signal (go) trials
+t2 <-
+  
+  d1 %>%
+  filter( signal == "nosignal" ) %>% # only nosignal/go trials considered
+  group_by( id, cond ) %>% # summarise for each patient/condition combination
+  summarise(
+    `p(correct)` = mean( acc[presp == 1] ), # accuracy, i.e. p(correct) (omitting trials without a response or anticpatory responses )
+    `p(miss)` = mean( miss ), # miss rate
+    RT = paste0( # response times for correct responses
+      round( mean( rt1[correct == "correct"], na.rm = T ), 0 ), " (",
+      round( sd( rt1[correct == "correct"], na.rm = T ), 0 ), ")"
+    )
+  ) %>%
+  
+  # tidy it up
+  pivot_wider( values_from = c(`p(correct)`, `p(miss)`, RT ), names_from = cond ) %>%
+  relocate( "p(miss)_ctrl", .after = "p(correct)_ctrl" ) %>%
+  relocate( "RT_ctrl", .after = "p(miss)_ctrl" )
