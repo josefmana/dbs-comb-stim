d9ce5cc6e359f5fac30480fd6eade77893f88cc6
Josef Mana <josef.mana@protonmail.com>
2024-07-23 22:09:13 +0200

    added a single participant with more data simulation

--- scripts/ExGaussian_fake_data_fitting.R
+++ scripts/ExGaussian_fake_data_fitting.R
@@ -29,7 +29,7 @@ reco_plot <- function(data, col = "red", tit = "Go Runner") data %>%
   ggplot() +
   aes(x = value) +
   geom_histogram(fill = "grey50") +
-  geom_vline( aes( xintercept = data$true_value ), colour = col, linewidth = 1.2 ) +
+  geom_vline( aes( xintercept = true_value ), colour = col, linewidth = 1.2 ) +
   facet_grid2(ID ~ parameter, scales = "free", independent = T) +
   labs(title = tit) +
   theme( plot.title = element_text(face = "bold", size = 14, hjust = .5) )
@@ -160,6 +160,7 @@ for ( i in 1:k ) {
 # plot trace plots, each for ca 3 s
 for (i in 1:k) {
 
+  print( paste0("participant #",i) )
   print( mcmc_trace( fit_many_indi[[i]]$draws(inc_warmup = T), n_warmup = 1e3 ) )
   Sys.sleep(3)
 
@@ -282,4 +283,156 @@ ppred <- lapply(
 # in the signal-respond trials, it makes little sense to "align" NAs between observed data and posterior predictions
 ppc_density(subset(d0$data, signal == 0), preds = ppred, cols = c("red4","lightpink"), tit = "GO TRIALS")
 ppc_density(subset(d0$data, signal == 1), preds = ppred, cols = c("blue4","lightblue"), tit = "SIGNAL-RESPOND TRIALS")
+
+
+# ONE PARTICIPANT/LOT OF DATA ----
+
+d1 <- ssrt_data_sim( N = 1, K = c(500,500) )
+
+summary(d1$data$rt)
+sum(d1$data$rt < 0, na.rm = T) / nrow(d1$data)
+
+d1$data %>%
+  
+  # some formatting shinanigans
+  filter( complete.cases(rt) ) %>%
+  mutate( `Response type: ` = ifelse( signal == "0", "GO", "SIGNAL-RESPOND" ) ) %>%
+  mutate( subject = paste0("Subject #",id) ) %>%
+  
+  # plot it
+  ggplot() +
+  aes(x = rt, fill = `Response type: `) +
+  geom_density(alpha = .5) +
+  labs(x = "Response time (s)", y = "Density") +
+  facet_wrap(~subject, ncol = 2) +
+  theme(legend.position = "bottom")
+
+# data
+dGO <- d1$data %>% filter( signal == 0 )
+dSR <- d1$data %>% filter( signal == 1 & !is.na(rt) )
+dNA <- d1$data %>% filter( signal == 1 & is.na(rt) )
+
+# input file
+dlist <- list(
+  
+  # data
+  Y_0_go = dGO$rt, N_0_go = nrow(dGO),
+  Y_0_sr = dSR$rt, N_0_sr = nrow(dSR), SSD_0_sr = dSR$ssd,
+  N_0_na = nrow(dNA), SSD_0_na = dNA$ssd,
+  
+  # priors
+  mu_go_0_p = c(-0.4,0.2), sigma_go_0_p = c(-2.0,0.2), lambda_go_0_p = c(-2.0,0.2),
+  mu_stop_0_p = c(-1.0,0.2), sigma_stop_0_p = c(-2.0,0.2), lambda_stop_0_p = c(-2.0,0.2)
+  
+)
+
+# fitting proper
+fit1 <- mod_indi$sample(data = dlist, chains = 4, save_warmup = T, init = ifun)
+
+mcmc_trace( fit1$draws(inc_warmup = T), n_warmup = 1e3 )
+
+real <- with(
+  
+  d1$parameters,
+  lapply(
+    
+    setNames( c("mu","sigma","lambda"), c("mu","sigma","lambda") ),
+    function(i)
+      
+      sapply(
+        1,
+        function(j)
+          c( go = get(i)[type == "global intercept" & racer == "go"] + get(i)[type == "subject-level variability" & racer == "go"] * get(i)[type == "varying effect" & racer == "go" & id == j],
+             stop = get(i)[type == "global intercept" & racer == "stop"] + get(i)[type == "subject-level variability" & racer == "stop"] * get(i)[type == "varying effect" & racer == "stop" & id == j]
+          )
+        
+      ) %>%
+      
+      as.data.frame() %>%
+      rownames_to_column("type") %>%
+      mutate( parameter = i, .before = 1 )
+  )
+) %>%
+  
+  do.call( rbind.data.frame, . ) %>%
+  pivot_longer( cols = starts_with("V"), names_to = "ID", values_to = "true_value" ) %>%
+  mutate( ID = sub("V","",ID) )
+
+estimated <- lapply(
+  
+  1,
+  function(i)
+    
+    fit1$draws(format = "data.frame") %>%
+    select( starts_with("Int"), .chain, .iteration ) %>%
+    mutate(ID = i, .before = 1)
+  
+) %>%
+  
+  do.call( rbind.data.frame, . ) %>%
+  pivot_longer( starts_with("Int"), values_to = "value", names_to = "name" ) %>%
+  mutate( name = sub("Int_", "", ( sub("_0","",name) ) ) ) %>%
+  mutate( type = sub(".*_", "", name), parameter = sub("_.*", "", name), ID = as.character(ID) )
+
+pars <-
+  left_join( estimated, real, by = c("ID","type","parameter") ) %>%
+  mutate( parameter = factor(parameter, levels = c("mu","sigma","lambda"), ordered = T) )
+
+
+library(patchwork)
+
+reco_plot(subset(pars, type == "go"), col = "red", tit = "Go Runner") /
+  reco_plot(subset(pars, type == "stop"), col = "blue", tit = "Stop Runner")
+
+ppred <- lapply(
+  
+  1, # one for each participant
+  function(i) {
+    
+    # extract posterior draws for subject i
+    df <-
+      subset(estimated, ID == i) %>%
+      select(.chain, .iteration, name, value) %>%
+      pivot_wider(names_from = name, values_from = value)
+    
+    sapply(
+      
+      1:nrow(df),
+      function(j) {
+        
+        print( paste0("Participant #",i,", sample #",j) )
+        
+        return( with(
+          
+          df,
+          ssrt_data_sim(
+            alpha_go = c(mu_go[j], 0),
+            alpha_stop = c(mu_stop[j], 0),
+            beta_go = c(sigma_go[j], 0),
+            beta_stop = c(sigma_stop[j], 0),
+            gamma_go = c(lambda_go[j], 0),
+            gamma_stop = c(lambda_stop[j], 0),
+            tau_go = c(0, 0),
+            tau_stop = c(0, 0),
+            zeta_go = c(0, 0),
+            zeta_stop = c(0, 0),
+            epsilon_go = c(0, 0),
+            epsilon_stop = c(0, 0),
+            N = 1,
+            df = subset(d1$data, id == i) %>% mutate(id = 1, rt = NA)
+          )$data$rt
+          
+        ) )
+        
+      }
+      
+    ) %>% t()
+    
+    
+  }
+)
+
+ppc_density(subset(d1$data, signal == 0), preds = ppred, cols = c("red4","lightpink"), tit = "GO TRIALS") /
+  ppc_density(subset(d1$data, signal == 1), preds = ppred, cols = c("blue4","lightblue"), tit = "SIGNAL-RESPOND TRIALS")
+
 
